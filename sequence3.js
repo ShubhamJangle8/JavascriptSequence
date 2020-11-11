73818dd91dc2330186564ba4971bbdab4170b943
jangle <shubhamjangale0000@gmail.com>
2020-11-11 21:22:17 +0530

    Added : Added functionality to get a 2 dice random inputs and find its sum

---
+++ sequence3.js
@@ -0,0 +1,6 @@
+//Random function to print two dice numbers
+let dice1 = Math.floor((Math.random() * 6) + 1);
+console.log("Dice 1 shows " + dice1);
+let dice2 = Math.floor((Math.random() * 6) + 1);
+console.log("Dice 2 shows " + dice2);
+console.log("Sum is : " + (dice1 + dice2));
