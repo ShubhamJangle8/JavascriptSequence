1e2a189e3ac3789db308234f6989d04079545901
jangle <shubhamjangale0000@gmail.com>
2020-11-11 21:31:27 +0530

    Added : Added functionality to convert from one unit to other

---
+++ sequence5.js
@@ -0,0 +1,9 @@
+//Unit conversion
+let foot = 12;
+let inch = 42;
+console.log(inch + " inch : " + (inch / foot));
+let area = 2400 * 0.3048;
+console.log("Rectangular plot of 60 * 40 ft : " + area);
+let noOfPlots = 25;
+let totalArea = (area * noOfPlots) / 4046.86;
+console.log("Area of 25 such plots: " + totalArea + " acres");
